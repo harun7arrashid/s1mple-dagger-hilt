acd666d8e4641daa405ec54094cad11622547488
harun7arrashid <harun4arrashid@gmail.com>
2021-10-30 17:02:39 +0700

    First Commit

---
+++ app/src/androidTest/java/id/s1mple/car/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package id.s1mple.car
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("id.s1mple.car", appContext.packageName)
+    }
+}
